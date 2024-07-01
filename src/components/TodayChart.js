08a7401df5ba74fd1dce7088b7dfc2a4461724f3
DaVinci-R <benbu134@qq.com>
2024-07-01 13:39:53 +0800

    Save changes before switching branches

--- src/components/TodayChart.js
+++ src/components/TodayChart.js
@@ -12,14 +12,13 @@ const TodayChart = () => {
                     This is where today's Power BI chart will be displayed.
                 </Typography>
                 {/* Embed Power BI chart here */}
-                <iframe 
-                title="XGboostAA_SM预测" 
+                <iframe title="XGboostAA_SM预测" 
                 width="1000" 
                 height="800" 
-                src="https://app.powerbi.com/view?r=eyJrIjoiNDZlNjdmZTctYWM4NC00MTY5LWE1Y2MtNDM2NzI5MGMyNTg1IiwidCI6IjI2Y2NmYmI0LTc4MTYtNGY0My1hMjM2LWI2ZmZmYjg0Y2ZjMSIsImMiOjEwfQ%3D%3D"
-                 frameborder="0" 
-                 allowFullScreen="true"
-                 ></iframe>
+                src="https://app.powerbi.com/view?r=eyJrIjoiOTliNTljZWUtMDI3NC00NDUzLWIyNjQtOGI1YzQ5MmQ2MDM0IiwidCI6IjI2Y2NmYmI0LTc4MTYtNGY0My1hMjM2LWI2ZmZmYjg0Y2ZjMSIsImMiOjEwfQ%3D%3D" 
+                frameborder="0" 
+                allowFullScreen="true"
+                ></iframe>
             </CardContent>
         </Card>
     );
