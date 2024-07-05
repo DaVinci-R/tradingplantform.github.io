a8bd13ae055e5dd32e5592b610e7fdcc16c8f5a5
DaVinci-R <benbu134@qq.com>
2024-07-05 09:45:20 +0800

    Deploy updated content to GitHub Pages

--- src/components/TodayChart.js
+++ src/components/TodayChart.js
@@ -12,10 +12,10 @@ const TodayChart = () => {
                     This is where today's Power BI chart will be displayed.
                 </Typography>
                 {/* Embed Power BI chart here */}
-                <iframe title="XGboostAA_SM预测" 
+                <iframe title="XGboost基差预测" 
                 width="1000" 
                 height="800" 
-                src="https://app.powerbi.com/view?r=eyJrIjoiOTliNTljZWUtMDI3NC00NDUzLWIyNjQtOGI1YzQ5MmQ2MDM0IiwidCI6IjI2Y2NmYmI0LTc4MTYtNGY0My1hMjM2LWI2ZmZmYjg0Y2ZjMSIsImMiOjEwfQ%3D%3D" 
+                src="https://app.powerbi.com/view?r=eyJrIjoiNGUxNmZkMDgtZWU4Ny00NmMwLTlhZjgtZjMxZWZmNDQyMWM5IiwidCI6IjI2Y2NmYmI0LTc4MTYtNGY0My1hMjM2LWI2ZmZmYjg0Y2ZjMSIsImMiOjEwfQ%3D%3D" 
                 frameborder="0" 
                 allowFullScreen="true"
                 ></iframe>
