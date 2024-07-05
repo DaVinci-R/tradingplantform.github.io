ae49cb3ba53252ebcd97a99d1eed6e2ee3f2dbb6
DaVinci-R <benbu134@qq.com>
2024-07-05 09:52:17 +0800

    Update TodayChart.js with new chart

--- src/components/TodayChart.js
+++ src/components/TodayChart.js
@@ -6,16 +6,16 @@ const TodayChart = () => {
         <Card>
             <CardContent>
                 <Typography variant="h5" component="div">
-                    Today's Chart
+                    今日基差预测
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                     This is where today's Power BI chart will be displayed.
                 </Typography>
-                {/* Embed Power BI chart here */}
-                <iframe title="XGboostAA_SM预测" 
+                {/* Embed Power BI chart here new */}
+                <iframe title="XGboost基差预测" 
                 width="1000" 
                 height="800" 
-                src="https://app.powerbi.com/view?r=eyJrIjoiOTliNTljZWUtMDI3NC00NDUzLWIyNjQtOGI1YzQ5MmQ2MDM0IiwidCI6IjI2Y2NmYmI0LTc4MTYtNGY0My1hMjM2LWI2ZmZmYjg0Y2ZjMSIsImMiOjEwfQ%3D%3D" 
+                src="https://app.powerbi.com/view?r=eyJrIjoiNGUxNmZkMDgtZWU4Ny00NmMwLTlhZjgtZjMxZWZmNDQyMWM5IiwidCI6IjI2Y2NmYmI0LTc4MTYtNGY0My1hMjM2LWI2ZmZmYjg0Y2ZjMSIsImMiOjEwfQ%3D%3D" 
                 frameborder="0" 
                 allowFullScreen="true"
                 ></iframe>
